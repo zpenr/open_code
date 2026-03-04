4219ab5b72f7f5e86bdf1f28c22211e71a67c79f
Myasnikov Vadim <zpenr@bk.ru>
2026-03-04 16:13:33 +0800

    Show test not grant you you from error principle

--- testing/src/math_demo.py
+++ testing/src/math_demo.py
@@ -1,2 +1,5 @@
 def add(a,b):
     return a+b
+
+def add_with_bug(a,b):
+    return a*b
--- testing/test/test_principles.py
+++ testing/test/test_principles.py
@@ -1,12 +1,18 @@
 import sys
 sys.path.append("../src")
 
-from math_demo import add
+from math_demo import add, add_with_bug
 
 def test_addtional():
     assert add(2,2) == 4
-    print("Test complite")
+    print("Test ADDITIONAL PASSED")
+
+def test_addtional_with_bug():
+    assert add_with_bug(2,2) == 4
+    assert add_with_bug(0,0) == 0
+    print("Test BUG ADDITIONAL PASSED (it's ok?)")
 
 if __name__ == "__main__":
     test_addtional()
+    test_addtional_with_bug()
 
