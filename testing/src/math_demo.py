0b5f9cdb5d525870124348da37f76501c57a1ffd
Myasnikov Vadim <zpenr@bk.ru>
2026-03-04 17:01:10 +0800

    Show pesticised

--- testing/src/math_demo.py
+++ testing/src/math_demo.py
@@ -3,3 +3,9 @@ def add(a,b):
 
 def add_with_bug(a,b):
     return a*b
+
+def calculate_tax_with_bag(income):
+    return income*0.15
+
+def calculate_tax(income):
+    return int(income*0.15*100) / 100.0
