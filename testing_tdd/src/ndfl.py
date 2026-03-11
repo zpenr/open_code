f371b336d72c05b1510685c67f03783b49ae46f9
Myasnikov Vadim <zpenr@bk.ru>
2026-03-11 16:52:35 +0800

    All tests are green

--- testing_tdd/src/ndfl.py
+++ testing_tdd/src/ndfl.py
@@ -1,2 +1,7 @@
 def calculate_ndfl_tax(income):
-    return income*0.13
+    if income < 2_400_000:
+        return income*0.13
+    elif income < 5_000_000:
+        return 2_400_000*0.13 + (income- 2_400_000) * 0.15
+    elif income < 20_000_000:
+        return 2_400_000*0.13 + (2_600_000) * 0.15 + (income - 5_000_000) * 0.18
