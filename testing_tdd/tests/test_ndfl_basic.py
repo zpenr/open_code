e06b48ba7ae3c0770794a625b9c9cc9692ee0ce7
Myasnikov Vadim <zpenr@bk.ru>
2026-03-11 16:42:19 +0800

    Added test suite for basic calc

--- testing_tdd/tests/test_ndfl_basic.py
+++ testing_tdd/tests/test_ndfl_basic.py
@@ -1,4 +1,12 @@
 from ndfl import calculate_ndfl_tax
 
-def test_ndfl():
-    assert calculate_ndfl_tax() == None
+def test_ndfl_tier1():
+    assert calculate_ndfl_tax(500_000) == 65_000
+
+
+def test_ndfl_tier2():
+    assert calculate_ndfl_tax(4_000_000) == 512_000
+
+
+def test_ndfl_tier3():
+    assert calculate_ndfl_tax(10_000_000) == 1_602_000
